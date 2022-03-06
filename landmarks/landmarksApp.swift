942775bb937f70727a92775c5da668bd56bdab26
Arthur Chiao <16827192+acchiao@users.noreply.github.com>
2022-03-05 21:15:43 -0600

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Arthur Chiao on 3/5/22.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
